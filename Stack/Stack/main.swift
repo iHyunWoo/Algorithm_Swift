52cf84ef3353fac1fb006aa3dec45e6dac224f0f
iHyunWoo <jrwedo7@gmail.com>
2022-12-14 21:23:02 +0900

    BOJ10828

--- Stack/Stack/main.swift
+++ Stack/Stack/main.swift
@@ -3,9 +3,44 @@
 //  Stack
 //  https://www.acmicpc.net/problem/10828
 //  Created by 정현우 on 2022/12/14.
-//
+//  52ms
 
 import Foundation
 
-print("Hello, World!")
+let count = readLine().map {Int($0)!}!
+var myStack: [Int] = []
+var commandList: [String] = []
+
+for _ in 0..<count {
+	commandList.append(readLine()!)
+}
+
+for command in commandList {
+	switch command {
+	case "pop":
+		if myStack.count != 0 {
+			print(myStack.removeLast())
+		} else {
+			print("-1")
+		}
+	case "size":
+		print(myStack.count)
+	case "empty":
+		if myStack.count == 0 {
+			print("1")
+		} else {
+			print("0")
+		}
+	case "top":
+		if myStack.count != 0 {
+			print(myStack.last!)
+		} else {
+			print("-1")
+		}
+	default:
+		let splitedCommand = command.components(separatedBy: " ")
+		let number = Int(splitedCommand[1])!
+		myStack.append(number)
+	}
+}
 
